b793514edae92e272e67a080310695dd1529d8f9
Helder Ferreira <hmcfb@hotmail.com>
2024-06-03 04:49:22 +0100

    feat: feature completed

---
+++ src/app/snippets/new/error.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+type ErrorPageProps = {
+	error: Error;
+	reset: () => void;
+};
+
+export default function ErrorPage({ error, reset }: ErrorPageProps) {
+	return <div>{error.message}</div>;
+}
